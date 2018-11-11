ee02ae93c84307a4feaeec4cd99ffea3c05226ed
david_cool721@yahoo.com <david_cool721@yahoo.com>
2018-11-11 21:50:44 +0900

    first commit

---
+++ Assignment2/Data/BookDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Assignment2.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Data
+{
+    /// <summary>
+    /// The DB context that map to the database instance.
+    /// 
+    /// </summary>
+    public class BookDbContext : DbContext
+    {
+        /// <summary>
+        /// Initialize the instance of the dbcontext by passing in the dbcontext option
+        /// </summary>
+        /// <param name="options"></param>
+        public BookDbContext(DbContextOptions options)
+           : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        /// <summary>
+        /// Books
+        /// </summary>
+        public DbSet<Book> Books { get; set; }
+    }
+}
+
---
+++ Assignment2/Entities/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment2.Entities
+{
+    /// <summary>
+    /// Book Object class with the defined behavior
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// The unique Id of the book
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        /// <summary>
+        /// The name of the book
+        /// </summary>
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+    }
+    }
